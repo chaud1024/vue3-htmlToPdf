5a9d5f54492be0ab329fc2b72300cb86c3e2b950
bora kim <br_kim@kiotcom.co.kr>
2024-10-07 15:35:18 +0900

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Vue3HtmlToPdf from '@/components/Vue3HtmlToPdf.vue'
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <Vue3HtmlToPdf />
+  </main>
+</template>
